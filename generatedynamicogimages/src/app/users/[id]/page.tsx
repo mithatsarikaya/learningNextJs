0e37b5e6b2d6429bd43fa6dbe11db018ddf043a1
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-12-09 14:24:22 +0300

    tried generation of dynamic og images, time to deploy

--- generatedynamicogimages/src/app/users/[id]/page.tsx
+++ generatedynamicogimages/src/app/users/[id]/page.tsx
@@ -1,4 +1,34 @@
 import { getUserByID } from "@/lib/userFunctions";
+import { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.id;
+
+  // fetch data
+  // const product = await fetch(`https://.../${id}`).then((res) => res.json());
+  let user = await getUserByID(id);
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: user.email,
+    description: user.company.bs,
+
+    // openGraph: {
+    //   images: ["/some-specific-page-image.jpg", ...previousImages],
+    // },
+  };
+}
 
 const UserPage = async ({ params }: { params: { id: string } }) => {
   let userIDFromUrl = params.id;
